bd5e2313d2774e7275d7a25cd638a72208520666
Kojo Idrissa <kojo.idrissa@gmail.com>
2021-02-12 16:01:49 -0600

    Added Job ViewSet

--- src/sccApi/views.py
+++ src/sccApi/views.py
@@ -1,6 +1,9 @@
 from django.views.generic import ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from rest_framework import viewsets
+
 from .models import Job
+from . import serializers
 
 
 class UserHomeView(LoginRequiredMixin, ListView):
@@ -15,3 +18,11 @@ class JobDetail(LoginRequiredMixin, DetailView):
     model = Job
     slug_field = "uuid"
     template_name = "sccApi/job_detail.html"
+
+
+class JobViewSet(viewsets.ModelViewSet):
+    """A viewset for viewing and editing Job instances."""
+
+    queryset = Job.objects.all()
+    serializer_class = serializers.JobSerializer
+    ordering = "created"
