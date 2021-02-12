48de49aa21eeb0ac3a4e34012d24eeb96dcc4b08
Kojo Idrissa <kojo.idrissa@gmail.com>
2021-02-12 16:26:19 -0600

    registered JobViewSet urls

--- src/sccApi/urls.py
+++ src/sccApi/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 
+from rest_framework.routers import DefaultRouter
+
 from . import views
+
+router = DefaultRouter()
+router.register(r"jobs", views.JobViewSet, basename="job")
+
 app_name = 'sccApi'
 urlpatterns = [
     path('', views.UserHomeView.as_view(), name="user_home"),
     path("<slug>/", views.JobDetail.as_view(), name="job_detail"),
 ]
+
+urlpatterns += router.urls
