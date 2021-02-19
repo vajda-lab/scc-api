5df9b70cb514ced8c3cdf1613a369564d8b2c537
Kojo Idrissa <kojo.idrissa@gmail.com>
2021-02-19 10:19:00 -0600

    attempted routing update failed

--- src/user_app/urls.py
+++ src/user_app/urls.py
@@ -1,6 +1,15 @@
 from django.conf.urls import url
+
+from rest_framework.routers import DefaultRouter
+
 from . import views
+
+router = DefaultRouter()
+router.register(r"users", views.UserViewSet, basename="user")
+
 app_name = 'user_app'
 urlpatterns = [
     url('', views.signup, name='signup'),
 ]
+
+urlpatterns += router.urls
