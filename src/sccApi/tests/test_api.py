bc3f389dd2adb7ff3d05f8dc66f37df958a883c6
Kojo Idrissa <kojo.idrissa@gmail.com>
2021-02-23 14:43:10 -0600

    updating sccApi API tests

--- src/sccApi/tests/test_api.py
+++ src/sccApi/tests/test_api.py
@@ -5,107 +5,107 @@ from model_bakery import baker
 from sccApi import models, serializers
 
 
-# Test User List url
+# Test Job List url
 @pytest.mark.django_db()
-def test_user_list_url(tp, user):
-    expected_url = "/apis/users/"
-    reversed_url = tp.reverse("user-list")
+def test_job_list_url(tp, job):
+    expected_url = "/apis/jobs/"
+    reversed_url = tp.reverse("job-list")
     assert expected_url == reversed_url
 
 
 @pytest.mark.django_db()
-def test_user_list(tp, user):
+def test_job_list(tp, job):
     """
-    GET '/apis/users/'
+    GET '/apis/jobs/'
     2nd assert is an extra check
     """
-    url = tp.reverse("user-list")
+    url = tp.reverse("job-list")
     response = tp.get_check_200(url)
     results = response.data["results"]
     assert len(results) == 1
 
     result = results[0]
-    assert user.pk == result["pk"]
+    assert str(job.pk) == result["uuid"]
 
 
 @pytest.mark.django_db()
-def test_user_create(tp):
+def test_job_create(tp):
     """
-    POST '/apis/users/'
+    POST '/apis/jobs/'
     """
-    url = tp.reverse("user-list")
-    user = baker.prepare("user_app.User")
-    payload = serializers.UserSerializer(instance=user).data
-    del payload["pk"]
+    url = tp.reverse("job-list")
+    job = baker.prepare("sccApi.Job")
+    payload = serializers.JobSerializer(instance=job).data
+    del payload["uuid"]
     response = tp.client.post(url, data=payload, content_type="application/json")
 
-    pk = response.json()["pk"]
+    pk = response.json()["uuid"]
     tp.response_201(response)
 
-    user_obj = models.User.objects.get(pk=pk)
-    assert user_obj.pk
+    job_obj = models.Job.objects.get(pk=pk)
+    assert job_obj.pk
 
 
-# Test User Detail url
+# Test Job Detail url
 @pytest.mark.django_db()
-def test_user_detail_url(tp, user):
-    expected_url = f"/apis/users/{user.pk}/"
-    reversed_url = tp.reverse("user-detail", pk=user.pk)
+def test_job_detail_url(tp, job):
+    expected_url = f"/apis/jobs/{job.pk}/"
+    reversed_url = tp.reverse("job-detail", pk=job.pk)
     assert expected_url == reversed_url
 
 
 @pytest.mark.django_db()
-def test_user_detail(tp, user):
+def test_job_detail(tp, job):
     """
-    GET '/apis/users/{pk}/'
+    GET '/apis/jobs/{pk}/'
     """
-    url = tp.reverse("user-detail", pk=user.pk)
+    url = tp.reverse("job-detail", pk=job.pk)
     response = tp.get_check_200(url)
-    assert "pk" in response.data
-    assert user.pk == response.data["pk"]
+    assert "uuid" in response.data
+    assert str(job.pk) == response.data["uuid"]
 
 
 @pytest.mark.django_db()
-def test_user_delete(tp):
+def test_job_delete(tp):
     """
-    DELETE '/apis/users/{pk}/'
+    DELETE '/apis/jobs/{pk}/'
     """
-    user = baker.make("user_app.User")
-    url = tp.reverse("user-detail", pk=user.pk)
+    job = baker.make("sccApi.Job")
+    url = tp.reverse("job-detail", pk=job.pk)
     response = tp.client.delete(url, content_type="application/json")
     tp.response_204(response)
-    assert models.User.objects.filter(pk=user.pk).count() == 0
+    assert models.Job.objects.filter(pk=job.pk).count() == 0
 
 
 @pytest.mark.django_db()
-def test_user_partial_update(tp, user):
+def test_job_partial_update(tp, job):
     """
-    PATCH '/apis/users/{pk}'
+    PATCH '/apis/jobs/{pk}'
     """
-    new_organization = "new-organization"
-    assert user.organization is not new_organization
-    url = tp.reverse("user-detail", pk=user.pk)
-    payload = {"organization": new_organization}
+    new_status = "new-status"
+    assert job.status is not new_status
+    url = tp.reverse("job-detail", pk=job.pk)
+    payload = {"status": new_status}
     response = tp.client.patch(url, data=payload, content_type="application/json")
     tp.response_200(response)
 
-    user_obj = models.User.objects.get(pk=user.pk)
-    assert user_obj.organization == new_organization
+    job_obj = models.Job.objects.get(pk=job.pk)
+    assert job_obj.status == new_status
 
 
 @pytest.mark.django_db()
-def test_user_update(tp, user):
+def test_job_update(tp, job):
     """
-    PUT '/apis/users/{pk}/'
+    PUT '/apis/jobs/{pk}/'
     """
-    new_organization = "new-organization"
-    assert user.organization is not new_organization
+    new_status = "new-status"
+    assert job.status is not new_status
 
-    url = tp.reverse("user-detail", pk=user.pk)
-    payload = serializers.UserSerializer(instance=user).data
-    payload["organization"] = new_organization
+    url = tp.reverse("job-detail", pk=job.pk)
+    payload = serializers.JobSerializer(instance=job).data
+    payload["status"] = new_status
     response = tp.client.put(url, data=payload, content_type="application/json")
     tp.response_200(response)
 
-    user_obj = models.User.objects.get(pk=user.pk)
-    assert user_obj.organization == new_organization
+    job_obj = models.Job.objects.get(pk=job.pk)
+    assert job_obj.status == new_status
