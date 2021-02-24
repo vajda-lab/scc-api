a290fe15a27711b3aec33e378bbfb438507415f8
Kojo Idrissa <kojo.idrissa@gmail.com>
2021-02-24 15:37:47 -0600

    updated fixture

--- src/user_app/tests/fixtures.py
+++ src/user_app/tests/fixtures.py
@@ -6,3 +6,13 @@ from model_bakery import baker
 @pytest.fixture
 def user():
     return baker.make("user_app.User")
+
+
+@pytest.fixture
+def superuser():
+    return baker.make("user_app.User", is_superuser=True, is_staff=True)
+
+
+@pytest.fixture
+def staff():
+    return baker.make("user_app.User", is_staff=True)
