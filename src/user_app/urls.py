a7f9cf2e76893b94d303727727514bb524df9045
Kojo Idrissa <kojo.idrissa@gmail.com>
2021-02-24 16:28:07 -0600

    user_app still needs a root template

--- src/user_app/urls.py
+++ src/user_app/urls.py
@@ -1,17 +1,10 @@
 from django.conf.urls import url
 
-from rest_framework.routers import DefaultRouter
 
 from . import views
 
-router = DefaultRouter()
-router.register(r"api/users", views.UserViewSet, basename="user")
-
 app_name = 'user_app'
 urlpatterns = [
-    # url('', views.signup, name='signup'),
-    url('', views.UserListView.as_view(), name='list-user'),
     url('signup/', views.signup, name='signup'),
+    url('', views.UserListView.as_view(), name='list-user'),
 ]
-
-urlpatterns += router.urls
