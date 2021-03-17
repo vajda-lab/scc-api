17994c0ff4b4cf50939174abdfdc504cfdc67561
Kojo Idrissa <kojo.idrissa@gmail.com>
2021-03-17 16:12:24 -0500

    updated docstring & added test_create_job

--- src/sccApi/tests/test_tasks.py
+++ src/sccApi/tests/test_tasks.py
@@ -9,7 +9,7 @@ from user_app.models import User
 @pytest.mark.django_db()
 def test_delete_job():
     """
-    # ToDo UPDATE DOCSTRING
+    Tests task status properly updated by tasks.delete_job() 
     """
     job = baker.make("sccApi.Job",)
     assert job.status is not models.Job.STATUS_DELETED
@@ -19,7 +19,14 @@ def test_delete_job():
 
 @pytest.mark.django_db()
 def test_create_job():
-    pass
+    """
+    Tests task status properly updated by tasks.create_job() 
+    """
+    job = baker.make("sccApi.Job",)
+    assert job.status is not models.Job.STATUS_ACTIVE
+    tasks.create_job(job.pk)
+    job.refresh_from_db()
+    assert job.status == models.Job.STATUS_ACTIVE
 
 @pytest.mark.django_db()
 def test_update_job_priority():
