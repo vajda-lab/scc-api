4d3e95eadadbbaeb7e10474e669c6d7e152e103a
Kojo Idrissa <kojo.idrissa@gmail.com>
2021-03-18 11:43:57 -0500

    adjusting task until model updated

--- src/sccApi/tasks.py
+++ src/sccApi/tasks.py
@@ -24,6 +24,9 @@ def delete_job(self, pk):
 def update_job_priority(self, pk):
     print(f"update_job_priority({pk})")
     job = Job.objects.get(pk=pk)
+    #until Job model has Priority field, task changes status to error
+    job.status = Job.STATUS_ERROR
+    job.save()
     # ToDo: use subprocess() to run {command to change job priority} on the submit host
     # ToDo: later, add priority field to Job model & update job status to change priority
     # ToDo: https://github.com/tveastman/secateur/blob/master/secateur/settings.py#L241-L245
