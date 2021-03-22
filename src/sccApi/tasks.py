03f24eaa7aecec433e5da22efd4aef7430317635
Kojo Idrissa <kojo.idrissa@gmail.com>
2021-03-22 16:03:40 -0500

    Now I understand this

--- src/sccApi/tasks.py
+++ src/sccApi/tasks.py
@@ -31,7 +31,7 @@ def update_job_priority(self, pk):
     # ToDo: use subprocess() to run {command to change job priority} on the submit host
     # ToDo: https://github.com/tveastman/secateur/blob/master/secateur/settings.py#L241-L245
         # Do we need to explicitly create separate queues in settings?
-    # ToDo: We'll need to pass job.priority into this task, if we add a priority field to Job (this comment doesn't make sense)
+    # ToDo: We'll need to pass job.priority into this task, if we add a priority field to Job (this comment doesn't make sense); Comment makes sense if more than 2 priority levels
 
 
 
