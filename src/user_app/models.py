473ab6974ffe3b7970f113d39d955207689d0f9b
Kojo Idrissa <kojo.idrissa@gmail.com>
2021-03-24 16:18:54 -0500

    added priority to User

--- src/user_app/models.py
+++ src/user_app/models.py
@@ -1,14 +1,15 @@
 from django.db import models
+from django.utils import timezone
 from improved_user.model_mixins import AbstractUser
-from sccApi.models import Job
+from sccApi.models import Job, Priority
 import datetime
 
-Affiliation_Choices = (
+
+AFFILIATION_CHOICES = (
     ("I", "Industry"),
     ("A", "Academia"),
     ("O", "Other"),
 )
-import datetime
 
 # yesterday = datetime.date.today() - datetime.timedelta(days=1)
 # orders = Order.objects.filter(date__gt=yesterday)
@@ -19,15 +20,16 @@ class User(AbstractUser):
     """A User model that extends the Improved User"""
 
     affiliation = models.CharField(
-        max_length=1, choices=Affiliation_Choices, default="O", null=False
+        max_length=1, choices=AFFILIATION_CHOICES, default="O", null=False
     )
     organization = models.CharField(max_length=250)
     notes = models.CharField(max_length=1000, blank=True, null=True)
     max_job_submission = models.IntegerField(default=20)
+    priority = models.IntegerField(choices=Priority.choices, default=Priority.LOW,)
 
     def daily_job_count(self):
         return (
             Job.objects.filter(user=self.pk)
-            .filter(created_at__gt=datetime.date.today())
+            .filter(created_at__gt=timezone.now())
             .count()
         )
