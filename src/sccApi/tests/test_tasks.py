53eaf16ac45baee30f9ae4e4837c478c9e2dc478
Kojo Idrissa <kojo.idrissa@gmail.com>
2021-03-29 08:34:47 -0500

    delete before create was bothering me

--- src/sccApi/tests/test_tasks.py
+++ src/sccApi/tests/test_tasks.py
@@ -7,17 +7,6 @@ from user_app.models import User
 
 
 @pytest.mark.django_db()
-def test_delete_job():
-    """
-    Tests task status properly updated by tasks.delete_job() 
-    """
-    job = baker.make("sccApi.Job",)
-    assert job.status != models.Job.STATUS_DELETED
-    tasks.delete_job(job.pk)
-    job.refresh_from_db()
-    assert job.status == models.Job.STATUS_DELETED
-
-@pytest.mark.django_db()
 def test_create_job():
     """
     Tests task status properly updated by tasks.create_job() 
@@ -29,6 +18,17 @@ def test_create_job():
     assert job.status == models.Job.STATUS_ACTIVE
     assert b"5290723" in qsub_status
     assert b"has been submitted" in qsub_status
+
+@pytest.mark.django_db()
+def test_delete_job():
+    """
+    Tests task status properly updated by tasks.delete_job() 
+    """
+    job = baker.make("sccApi.Job",)
+    assert job.status != models.Job.STATUS_DELETED
+    tasks.delete_job(job.pk)
+    job.refresh_from_db()
+    assert job.status == models.Job.STATUS_DELETED
 
 @pytest.mark.django_db()
 def test_update_job_priority():
