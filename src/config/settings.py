5b58a8e75e155eedbe702b0afab84fa8134610fb
Jeff Triplett <jeff.triplett@gmail.com>
2021-04-19 14:15:16 -0500

    :gear: Adjusts test settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -87,9 +87,8 @@ DATABASES = {
 }
 # dashboard database for `django-sql-dashboard`
 DATABASES["dashboard"] = DATABASES["default"].copy()
-DATABASES["dashboard"]["OPTIONS"] = {
-    "options": "-c default_transaction_read_only=on -c statement_timeout=100"
-}
+DATABASES["dashboard"]["OPTIONS"] = {"options": "-c default_transaction_read_only=on -c statement_timeout=100"}
+DATABASES["dashboard"]["TEST"] = {"LEGACY": True}
 
 
 CONSTANCE_REDIS_CONNECTION = "redis://redis:6379/0"
