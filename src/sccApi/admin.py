cca58e1010858702900e4a453ed23a44e12533ce
Jeff Triplett <jeff.triplett@gmail.com>
2021-04-22 13:49:55 -0500

    :sparkles: Adds JobLogAdmin

--- src/sccApi/admin.py
+++ src/sccApi/admin.py
@@ -9,3 +9,9 @@ class JobAdmin(admin.ModelAdmin):
 
 
 admin.site.register(models.Job, JobAdmin)
+@admin.register(models.JobLog)
+class JobLogAdmin(admin.ModelAdmin):
+    date_hierarchy = "created"
+    list_display = ["job", "event", "created"]
+    raw_id_fields = ["job"]
+    search_fields = ["event"]
