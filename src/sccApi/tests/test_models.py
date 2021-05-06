e05ff1a9d11fd734a132325b552d44877e75c15c
Jeff Triplett <jeff.triplett@gmail.com>
2021-05-05 20:35:44 -0500

    :green_heart: Adds job_log support and test

--- src/sccApi/tests/test_models.py
+++ src/sccApi/tests/test_models.py
@@ -4,6 +4,11 @@ from ..models import Priority, Status
 
 
 @pytest.mark.django_db()
-def test_model_defaults(job):
+def test_job_model_defaults(job):
     assert job.priority == Priority.LOW
     assert job.status == Status.QUEUED
+
+
+@pytest.mark.django_db()
+def test_job_log_model_defaults(job_log):
+    assert job_log.created
