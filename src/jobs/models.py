e8b556bdd199970e1018fec0965dce3936efea93
Kojo Idrissa <kojo.idrissa@gmail.com>
2021-06-10 14:50:39 -0500

    new users get tokens

--- src/jobs/models.py
+++ src/jobs/models.py
@@ -4,6 +4,15 @@ from django.core.validators import FileExtensionValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
 
 class Priority(models.IntegerChoices):
     LOW = (0, "low")
