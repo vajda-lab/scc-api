b02b093a95d1385c9ebc8d1e95c099bc49f4efed
Kojo Idrissa <kojo.idrissa@gmail.com>
2021-06-10 14:52:58 -0500

    ran isort

--- src/jobs/models.py
+++ src/jobs/models.py
@@ -1,13 +1,13 @@
 import uuid
 
+from django.conf import settings
 from django.core.validators import FileExtensionValidator
 from django.db import models
-from django.utils.translation import gettext_lazy as _
-
-from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.utils.translation import gettext_lazy as _
 from rest_framework.authtoken.models import Token
+
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
