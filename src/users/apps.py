31d3f13d09d1cb7439c1d326fc86e9bf5ae471c3
Kojo Idrissa <kojo.idrissa@gmail.com>
2021-06-15 11:05:52 -0500

    update based on docs

--- src/users/apps.py
+++ src/users/apps.py
@@ -1,10 +1,10 @@
 from django.apps import AppConfig
 from django.conf import settings
-from .signals import create_auth_token
 
 class UserAppConfig(AppConfig):
     name = "users"
 
 
     def ready(self):
-        create_auth_token(sender=settings.AUTH_USER_MODEL)
+        from .signals import create_auth_token
+        create_auth_token()
