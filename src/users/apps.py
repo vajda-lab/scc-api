79deeecb3bfe9bf667dc1d5dfea30578b6d9050e
Kojo Idrissa <kojo.idrissa@gmail.com>
2021-06-15 14:21:05 -0500

    ran black

--- src/users/apps.py
+++ src/users/apps.py
@@ -1,9 +1,9 @@
 from django.apps import AppConfig
 from django.conf import settings
 
+
 class UserAppConfig(AppConfig):
     name = "users"
 
-
     def ready(self):
-        from . import signals # noqa
+        from . import signals  # noqa
