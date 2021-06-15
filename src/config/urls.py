b43bcf989ef29270ebc5a412c54d09f98b1a00a7
Jeff Triplett <jeff.triplett@gmail.com>
2021-06-15 15:27:21 -0500

    :pencil: Adds version support to admin url

--- src/config/urls.py
+++ src/config/urls.py
@@ -22,6 +22,7 @@ from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
 
+from . import __version__
 from . import views
 from jobs.views import JobViewSet
 from users.views import UserViewSet
@@ -30,6 +31,10 @@ router = DefaultRouter()
 router.register("jobs", JobViewSet, basename="job")
 router.register("users", UserViewSet, basename="user")
 
+admin_header = f"Vajda Lab SCC API v{__version__}"
+admin.site.enable_nav_sidebar = False
+admin.site.site_header = admin_header
+admin.site.site_title = admin_header
 
 urlpatterns = [
     path("admin/", admin.site.urls),
