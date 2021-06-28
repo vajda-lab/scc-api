0975db17ba4159198abd19c55a1a3556cc888caf
Jeff Triplett <jeff.triplett@gmail.com>
2021-06-28 16:05:29 -0500

    :sparkles: Expose access token to user homepage

--- src/config/views.py
+++ src/config/views.py
@@ -1,5 +1,13 @@
 from django.views.generic import TemplateView
 
+from rest_framework.authtoken.models import Token
+
 
 class HomeView(TemplateView):
     template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.user.is_authenticated:
+            context["tokens"] = Token.objects.filter(user=self.request.user)
+        return context
