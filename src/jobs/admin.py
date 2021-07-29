8cdbf90a4b77d3eb77acaf35d8d9474a3f54e15e
Jeff Triplett <jeff.triplett@gmail.com>
2021-07-29 08:38:19 -0500

    :nail_care: Admin polish

--- src/jobs/admin.py
+++ src/jobs/admin.py
@@ -15,7 +15,8 @@ class JobAdmin(admin.ModelAdmin):
         JobLogInline,
     ]
     list_display = [
-        "uuid",
+        "get_uuid_field",
+        "user",
         "imported",
         "sge_task_id",
         "status",
@@ -34,6 +35,12 @@ class JobAdmin(admin.ModelAdmin):
     raw_id_fields = ["user"]
     readonly_fields = ["created", "modified"]
     search_fields = ["uuid", "sge_task_id"]
+
+    def get_uuid_field(self, obj):
+        # UUIDs are big. Let's just show the last section to trim the size down.
+        return str(obj.uuid).split("-")[-1]
+
+    get_uuid_field.short_description = "UUID"
 
 
 @admin.register(models.JobLog)
