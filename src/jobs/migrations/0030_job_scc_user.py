07b0773d6ab43514cd39aaa357aade425fe9d358
Jeff Triplett <jeff.triplett@gmail.com>
2021-08-26 15:13:51 -0500

    :shirt: Lints via Black

--- src/jobs/migrations/0030_job_scc_user.py
+++ src/jobs/migrations/0030_job_scc_user.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('jobs', '0029_auto_20210729_1635'),
+        ("jobs", "0029_auto_20210729_1635"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='job',
-            name='scc_user',
+            model_name="job",
+            name="scc_user",
             field=models.CharField(blank=True, db_index=True, max_length=20),
         ),
     ]
--- src/jobs/migrations/0031_alter_job_sge_task_id.py
+++ src/jobs/migrations/0031_alter_job_sge_task_id.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('jobs', '0030_job_scc_user'),
+        ("jobs", "0030_job_scc_user"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='job',
-            name='sge_task_id',
+            model_name="job",
+            name="sge_task_id",
             field=models.IntegerField(blank=True, db_index=True, null=True),
         ),
     ]
