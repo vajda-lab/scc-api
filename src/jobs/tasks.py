db557b9f9e6d412976059f679a0b717474430741
gj85369 <46541107+gj85369@users.noreply.github.com>
2022-01-04 16:26:31 -0500

    Update tasks.py

--- src/jobs/tasks.py
+++ src/jobs/tasks.py
@@ -35,7 +35,8 @@ def activate_job(self: celery.Task, *, pk: typing.Union[str, uuid.UUID]):
     called via: `scheduled_allocate_job`
 
     """
-    logging.info('here')
+    #logging.info('here')
+    logging.info('here')    
     try:
         job = Job.objects.get(pk=pk)
 
