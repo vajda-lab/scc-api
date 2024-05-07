a033f68093582aa02ebc2fd306c22e27d57e1c86
gj85369 <46541107+gj85369@users.noreply.github.com>
2024-05-07 10:23:34 -0400

    Update tasks.py
    wait till tar is done

--- src/jobs/tasks.py
+++ src/jobs/tasks.py
@@ -317,6 +317,11 @@ def scheduled_capture_job_output(self: celery.Task) -> None:
                 "-C",
                 f"{ftplus_path}",
                 ".",
+                "&&",
+                "echo",
+                "'this is done now'",
+                ">>",
+                "log"
             ]
 
             logger.debug(f"File Retrival Command: {cmd}")
